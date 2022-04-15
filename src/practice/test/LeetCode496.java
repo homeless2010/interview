8c2bfff933e422f9289d485d61469a89defe7481
homeless2010 <15594821882@163.com>
2022-04-15 20:06:50 +0800

    Add files via upload

---
+++ src/practice/test/LeetCode496.java
@@ -0,0 +1,27 @@
+package practice.test;
+
+import io.swagger.models.auth.In;
+
+import java.util.*;
+
+/**
+ * https://leetcode-cn.com/problems/next-greater-element-i/
+ */
+public class LeetCode496 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums2[i] >= stack.peek()) {
+                stack.pop();
+            }
+            map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
+            stack.push(nums2[i]);
+        }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; ++i) {
+            res[i] = map.get(nums1[i]);
+        }
+        return res;
+    }
+}
