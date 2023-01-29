30f929b91b8f154580c9391fb223aee1f8a0e66f
homeless2010 <paulvsjames@gmail.com>
2023-01-29 22:10:08 +0800

    feat: Matrix_29

---
+++ src/main/java/com/cjcui/leetcode/Matrix_29.java
@@ -0,0 +1,110 @@
+package com.cjcui.leetcode;
+
+/**
+ * 螺旋矩阵
+ */
+public class Matrix_29 {
+    public int[] spiralOrder(int[][] matrix) {
+        if (matrix.length == 0) {
+            return new int[0];
+        }
+        int l = 0, r = matrix[0].length - 1, t = 0, b = matrix.length - 1, x = 0;
+        int[] res = new int[(r + 1) * (b + 1)];
+        while (true) {
+            for (int i = l; i <= r; i++) {
+                // left to right.
+                res[x++] = matrix[t][i];
+            }
+            if (++t > b) {
+                break;
+            }
+            for (int i = t; i <= b; i++) {
+                // top to bottom.
+                res[x++] = matrix[i][r];
+            }
+            if (l > --r) {
+                break;
+            }
+            for (int i = r; i >= l; i--) {
+                // right to left.
+                res[x++] = matrix[b][i];
+            }
+            if (t > --b) {
+                break;
+            }
+            for (int i = b; i >= t; i--) {
+                // bottom to top.
+                res[x++] = matrix[i][l];
+            }
+            if (++l > r) {
+                break;
+            }
+        }
+        return res;
+    }
+
+    public int[] spiralOrder2(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return new int[0];
+        }
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        boolean[][] visited = new boolean[rows][columns];
+        int total = rows * columns;
+        int[] order = new int[total];
+        int row = 0, column = 0;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int directionIndex = 0;
+        for (int i = 0; i < total; i++) {
+            order[i] = matrix[row][column];
+            visited[row][column] = true;
+            int nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
+            if (nextRow < 0 || nextRow >= rows || nextColumn < 0 || nextColumn >= columns || visited[nextRow][nextColumn]) {
+                directionIndex = (directionIndex + 1) % 4;
+            }
+            row += directions[directionIndex][0];
+            column += directions[directionIndex][1];
+        }
+        return order;
+    }
+
+    public int[] spiralOrder3(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return new int[0];
+        }
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int[] order = new int[rows * columns];
+        int index = 0;
+        int left = 0, right = columns - 1, top = 0, bottom = rows - 1;
+        while (left <= right && top <= bottom) {
+            for (int column = left; column <= right; column++) {
+                order[index++] = matrix[top][column];
+            }
+            for (int row = top + 1; row <= bottom; row++) {
+                order[index++] = matrix[row][right];
+            }
+            if (left < right && top < bottom) {
+                for (int column = right - 1; column > left; column--) {
+                    order[index++] = matrix[bottom][column];
+                }
+                for (int row = bottom; row > top; row--) {
+                    order[index++] = matrix[row][left];
+                }
+            }
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+        return order;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[] spiralOrder = new Matrix_29().spiralOrder3(matrix);
+        for (int i = 0; i < spiralOrder.length; i++) {
+            System.out.println(spiralOrder[i]);
+        }
+    }
+}
